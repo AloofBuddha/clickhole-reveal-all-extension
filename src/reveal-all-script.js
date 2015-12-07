e7d49330a51770accb878aa9ddf19b4f4f396278
Benjamin Cohen <bac1087@gmail.com>
2015-12-07 16:57:03 -0500

    added functionality to reveal individual answers explanations for quizzes that support that

--- src/reveal-all-script.js
+++ src/reveal-all-script.js
@@ -1,18 +1,27 @@
-console.log("ClickHole - Reveal All! is running on this page. To remove it, disable in chrome://extensions/");
+// colored console.log from http://stackoverflow.com/questions/7505623/colors-in-javascript-console
+console.log("%cClickHole - Reveal All!" + 
+	"%c is running on this page.\nTo remove it, disable in chrome://extensions/",
+	"background: #fe621e; color: #fff", "");
 
 var revealAllHtml = [
-	'<pre>or</pre>',
-	'<button id="revealall" ',
-		'class="btn btn-default btn-lg">',
+	'<button ',
+		'class="btn btn-default btn-lg reveal-all">',
 			'Reveal All!',
 	'</button>'
 	].join('');
 
-var form = $(".quiz > form").append(revealAllHtml);
+// always display on bottom
+$(".quiz > form").append('<pre>or</pre>', revealAllHtml);
 
-$("#revealall").click(function (event) {
+// display on top too if has answer-explanations
+if ( $(".answer-explanation").length ) {
+	$(".quiz > form").prepend(revealAllHtml);
+}
+
+$(".reveal-all").click(function (event) {
     event.preventDefault();
 
+	$(".answer-explanation").css("display", "block");
 	$(".outcomes").css("display", "block");
 	$(".outcome").css("display", "list-item");
 });
